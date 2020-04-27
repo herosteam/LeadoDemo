7c55f436b26c5e808b17ec261ce421da3afc8415
Michael Ghaly <mghaly1500087@gmail.com>
2020-04-27 18:10:50 +0200

    User_Data

--- app/src/main/java/com/example/leadoappdemo/models/User.kt
+++ app/src/main/java/com/example/leadoappdemo/models/User.kt
@@ -1,4 +1,11 @@
 package com.example.leadoappdemo.models
 
-class User {
-}
+data class User (
+    val Course1IDLesson1ID: String = "",
+    val Course1IDLesson2ID: String = "",
+    val EnrolledCourses: Int = 0,
+    val Name: String = "",
+    val NumOfCourses: Int = 0,
+    val Phone: String = "",
+    val Rank: String = ""
+)
