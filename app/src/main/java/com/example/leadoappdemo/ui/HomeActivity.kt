1ba95f6e4893b5805e162b62a45976b4d5d2dc4c
younis <herosteam369@gmail.com>
2020-04-27 07:34:57 +0200

    comment

--- app/src/main/java/com/example/leadoappdemo/ui/HomeActivity.kt
+++ app/src/main/java/com/example/leadoappdemo/ui/HomeActivity.kt
@@ -6,8 +6,12 @@ import com.example.leadoappdemo.R
 
 class HomeActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Home Activity with four fragments
     }
 }
