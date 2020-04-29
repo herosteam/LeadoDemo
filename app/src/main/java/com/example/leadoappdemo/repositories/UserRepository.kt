54eff60eeaa7252cf9dc0c33c80cc8d2a18d096a
Michael Ghaly <mghaly1500087@gmail.com>
2020-04-29 22:26:22 +0200

    repository

--- app/src/main/java/com/example/leadoappdemo/repositories/UserRepository.kt
+++ app/src/main/java/com/example/leadoappdemo/repositories/UserRepository.kt
@@ -1,4 +1,60 @@
 package com.example.leadoappdemo.repositories
 
-class UserRepository {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.leadoappdemo.models.User
+import com.google.firebase.firestore.FirebaseFirestore
+
+object UserRepository
+{
+
+    val userList = mutableListOf<User>()
+
+    //private var lastUpdate = 1
+
+    val db = FirebaseFirestore.getInstance()
+    val usersCollection = db.collection("User")
+
+    private val liveUserList: MutableLiveData<List<User>> = MutableLiveData()
+
+    fun loadUsers(): LiveData<List<User>>
+    {
+        loadDataFromRemote()
+        return liveUserList
+    }
+
+    /*private fun loadDataFromMemory(): Boolean
+    {
+        if(userList.size == 0)
+        {
+            return false
+        }
+        else{
+            lastUpdate++
+            return true
+        }
+
+    }*/
+
+
+    private fun loadDataFromRemote()
+    {
+        usersCollection.get()
+            .addOnSuccessListener {
+                Log.d("mike","from remote data source")
+                // add them to your db
+                for(document in it.documents)
+                {
+                    val user = document.toObject(User::class.java)
+                    Log.d("mike","${user?.Rank.toString()}")
+                    userList.add(user!!)
+                }
+                liveUserList.postValue(userList)
+            }
+            .addOnFailureListener {
+                Log.d("mike",it.message)
+            }
+    }
+
 }
