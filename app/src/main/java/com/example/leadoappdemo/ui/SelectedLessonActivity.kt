90645441a4f6f08d2e9c0306ea58af3a32a8bdc7
younis <herosteam369@gmail.com>
2020-04-30 02:52:53 +0200

    Lesson Xml

--- app/src/main/java/com/example/leadoappdemo/ui/SelectedLessonActivity.kt
+++ app/src/main/java/com/example/leadoappdemo/ui/SelectedLessonActivity.kt
@@ -3,11 +3,22 @@ package com.example.leadoappdemo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.leadoappdemo.R
+import com.google.android.exoplayer2.ExoPlayerFactory.newSimpleInstance
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import kotlinx.android.synthetic.main.activity_selected_lesson.*
 
 class SelectedLessonActivity : AppCompatActivity() {
+
+    val video_url="http://techslides.com/demos/sample-videos/small.mp4"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selected_lesson)
+
+
     }
 }
