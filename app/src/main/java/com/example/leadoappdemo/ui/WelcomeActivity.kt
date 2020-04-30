cbda18f4d4ef3778783a9835b905aec6b307f396
Michael Ghaly <mghaly1500087@gmail.com>
2020-05-01 00:44:42 +0200

    welcomeActivity

--- app/src/main/java/com/example/leadoappdemo/ui/WelcomeActivity.kt
+++ app/src/main/java/com/example/leadoappdemo/ui/WelcomeActivity.kt
@@ -1,13 +1,20 @@
 package com.example.leadoappdemo.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.leadoappdemo.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome);
+
+        button11.setOnClickListener {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
