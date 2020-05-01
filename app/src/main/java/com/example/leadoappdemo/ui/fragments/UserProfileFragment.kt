a026607d4cbb4a65d3c14695cf6d6d8c6193dfa7
HassanAlex <Hassan.elsayed.ammer@gmail.com>
2020-05-01 11:00:44 +0200

    Add new intent

--- app/src/main/java/com/example/leadoappdemo/ui/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/leadoappdemo/ui/fragments/UserProfileFragment.kt
@@ -1,17 +1,23 @@
 package com.example.leadoappdemo.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 import com.example.leadoappdemo.R
+import com.example.leadoappdemo.ui.HomeActivity
+import kotlinx.android.synthetic.main.control_app_bar.*
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class UserProfileFragment : Fragment() {
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +25,16 @@ class UserProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false)
+
+
+        iV_back.setOnClickListener {
+            var intent = Intent(context,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+
 
 }
